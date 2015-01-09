06fb3e88427f6ca05c3a54d2bfd590976e7bbe99
Anthony Liu <anthonyscottliu@gmail.com>
2015-01-08 16:47:29 -0800

    Calibrates the timer before starting anything else.

--- js/mocktrialclerktool.js
+++ js/mocktrialclerktool.js
@@ -10,6 +10,7 @@
 var MTClerkTool = (function() {
     /**********
      * config */
+    var calibTime = 15; //seconds spent calibrating
 
     /*************
      * constants */
@@ -17,75 +18,104 @@ var MTClerkTool = (function() {
     /*********************
      * working variables */
     var timers;
+    var calibratedDelay;
 
     /******************
      * work functions */
     function initMTClerkTool() {
-        //init timers
-        timers = [];
-        timers.push(
-            new Timer(61, function(self) {
-                $s('#time').innerHTML = self.format();
-            })
-        );
-        timers[0].start();
+        //calibrate the timers
+        testTimer(calibTime, function(g, a) {
+            var errPerSec = (a-g)/g;
+            calibratedDelay = 1000*(1-errPerSec);
 
-        //user control
-        $s('#start').addEventListener('click', function() {
+            //init timers
+            timers = [];
+            timers.push(
+                new Timer(61, calibratedDelay, function(self) {
+                    $s('#time').innerHTML = self.format();
+                })
+            );
             timers[0].start();
-        });
-        $s('#stop').addEventListener('click', function() {
-            timers[0].stop();
-        });
 
-        //tests
-        testTimer(5);
-        testTimer(10);
-        testTimer(60);
+            //user control
+            $s('#start').addEventListener('click', function() {
+                timers[0].start();
+            });
+            $s('#stop').addEventListener('click', function() {
+                timers[0].stop();
+            });
+
+            //tests
+            testTimer(5);
+            testTimer(10);
+            testTimer(60);
+        });
     }
 
-    function testTimer(g) { //g is goal in seconds
+    function testTimer(g, callback) { //g is goal in seconds
+        var gapLen = 3000; //ms gap inserted halfway
         var start = +new Date();
-        var tmr = new Timer(g, (function(s) {
+        var tmr = new Timer(g, 1000, (function(s) {
             return function(self) {
                 if (self.timeLeft === 0) {
-                    var actual = ((+new Date())-s)/1000;
-                    console.log(
-                        'Goal: '+g+'s; '+'Actual: '+actual
-                    );
+                    var actual = ((+new Date())-s)/1000 - gapLen/1000;
+                    if (callback) callback(g, actual);
+                    else {
+                        console.log(
+                            'Goal: '+g+'s; '+'Actual: '+actual
+                        );
+                    }
                 }
             };
         })(start));
         tmr.start();
+
+        setTimeout(function() {
+            tmr.stop();
+            setTimeout(function() {
+                tmr.start();
+            }, gapLen);
+        }, g/(2+0.3*Math.random()));
     }
 
     /***********
      * objects */
-    function Timer(t, f) {
+    function Timer(t, d, f) {
         this.timeLeft = t;
+        this.delay = d || 1000;
         this.func = f || false;
         this.locked = true;
+        this.secBeganAt = -1;
+        this.lockedAt = -1;
     }
     Timer.prototype.start = function() {
         var self = this;
         if (this.locked) { //only if it's currently stopped
             this.timeLeft += 1;
             this.locked = false;
-            self.countDown();
+            self.countDown(this.delay - (this.lockedAt - this.secBeganAt));
         }
     };
-    Timer.prototype.countDown = function() {
+    Timer.prototype.countDown = function(delay) {
+        delay = delay || this.delay;
+
         var self = this;
         if (this.timeLeft > 0 && !this.locked) {
+            this.secBeganAt = +new Date();
             this.timeLeft -= 1;
+            var funStart = +new Date();
             if (this.func) this.func(self);
+            var funEnd = +new Date();
             setTimeout(function() {
                 self.countDown();
-            }, 1000);
+            }, delay - (funEnd - funStart));
         }
     };
     Timer.prototype.stop = function() {
-        if (!this.locked) this.locked = true;
+        if (!this.locked) {
+            this.locked = true;
+            this.lockedAt = +new Date();
+        }
     };
     Timer.prototype.format = function() {
         var m = Math.floor(this.timeLeft/60);
